437297357aaace3aeefcb217094b6f4b0e212a90
Flu3nt <gnedyalkov94@gmail.com>
2022-08-24 23:54:14 +0300

    Filtered out the prices of each of the top listings, wrote a function to calculate average prices

--- index.js
+++ index.js
@@ -16,6 +16,39 @@ async function getOneBedroomApartments() {
   const apartment = $("#adrows_8216642");
   // Select all the apartments in the content container
   const apartmentsContainer = $("#content_container");
+
+  // Select all listing apartments
+  const topListingApartments = apartmentsContainer
+    .find(".listtop-item")
+    .each((i, element) => {
+      const $element = $(element);
+      const topListing = {};
+      // get the titles of all listings
+      topListing.title = $element.find(".listtop-item-title").text();
+
+      // find the parameters of all listings
+      topListing.price = $element.find(".ads-params-single").text();
+
+      // select only the price of the listing
+      const topListingParameters = topListing.price.split(" ");
+
+      const topListingPrices = topListingParameters[0];
+
+      const pricesStrings = [];
+      pricesStrings.push(topListingPrices.slice(0, 6).replace(/\s+/g, ""));
+
+      const prices = pricesStrings.map((price) => Number(price));
+    });
 }
 
 getOneBedroomApartments();
+
+function calculateAverageValue(arr) {
+  let numberOfElements = arr.length;
+
+  let sumElements = arr.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue;
+  });
+
+  return sumElements / numberOfElements;
+}
