df6662b818f1d41d6e0dafa60a317bc8d81179d9
Flu3nt <gnedyalkov94@gmail.com>
2022-10-05 17:09:29 +0300

    Refactor code

--- test/src/scraper.js
+++ test/src/scraper.js
@@ -2,88 +2,69 @@ import axios from "axios"
 import { load } from "cheerio"
 import parseInput from "./utils.js"
 
-const urlOneBedroomApartments =
-  "https://www.alo.bg/obiavi/imoti-prodajbi/apartamenti-stai/?region_id=2&location_ids=300&section_ids=23&p[413]=1574"
-
-const topListings = []
-const vipListings = []
-
-async function getOneBedroomApartments(url) {
+async function getApartments(url) {
   // request the data (html) from the url using axios
   const { data } = await axios.get(url)
   // Load the html
   const $ = load(data)
 
+  const topListings = []
+  const vipListings = []
+
   // Select all the apartments in the content container
   const apartmentsContainer = $("#content_container")
 
-  const topListingApartments = apartmentsContainer
-    .find(".listtop-item")
-    .each((i, element) => {
-      const $element = $(element)
-      const topListing = {}
+  apartmentsContainer.find(".listtop-item").each((i, element) => {
+    const $element = $(element)
+    const topListing = {}
 
-      topListing.title = $element.find(".listtop-item-title").text()
-      const parameters = $element.find(".ads-params-single").text()
-      topListing.parameters = parseInput(parameters)
+    topListing.title = $element.find(".listtop-item-title").text()
+    const parameters = $element.find(".ads-params-single").text()
 
-      topListings.push(topListing)
-    })
+    topListing.parameters = parseInput(parameters)
+    topListings.push(topListing)
+  })
 
-  const vipListingsApartments = apartmentsContainer
-    .find(".listvip-params")
-    .each((i, listing) => {
-      const $listing = $(listing)
-      const vipListing = {}
+  apartmentsContainer.find(".listvip-params").each((i, listing) => {
+    const $listing = $(listing)
+    const vipListing = {}
 
-      vipListing.title = $listing
-        .find(".listvip-item-header")
-        .text()
-        .replace(/\t/gm, "")
-        .replace(/\n/gm, "")
+    vipListing.title = $listing
+      .find(".listvip-item-header")
+      .text()
+      .replace(/\t/gm, "")
+      .replace(/\n/gm, "")
 
-      vipListing.parameters = $listing
-        .find(".listvip-item-content")
-        .text()
-        .replace(/\n/g, "")
-        .replace(/\t/g, "")
+    vipListing.parameters = $listing
+      .find(".listvip-item-content")
+      .text()
+      .replace(/\n/g, "")
+      .replace(/\t/g, "")
 
-      console.log(vipListing.parameters)
-      console.log("------------")
+    vipListing.parameters = parseInput(vipListing.parameters)
+    vipListings.push(vipListing)
+  })
 
-      vipListing.parameters = parseInput(vipListing.parameters)
-
-      vipListings.push(vipListing)
-    })
-
-  return await [...topListings, ...vipListings]
+  return [...topListings, ...vipListings]
 }
 
 // get the first page
 async function getFirstPage(url) {
-  getOneBedroomApartments(url).then((data) => {
+  getApartments(url).then((data) => {
     console.log(data)
-    console.log(data.length)
     return data
   })
 }
-// get all pages
-async function getAllProperties(url) {
-  let data = []
-  let page = 2
-  let onebedUrl = `https://www.alo.bg/obiavi/imoti-prodajbi/apartamenti-stai/?region_id=2&location_ids=300&section_ids=23&p[413]=1574&page=${page}`
-  while (page <= 6) {
-    data = [...(await getOneBedroomApartments(url))]
-    page++
-  }
-  return data[1]
-}
 
-let twobedUrl = `https://www.alo.bg/obiavi/imoti-prodajbi/apartamenti-stai/?region_id=2&location_ids=300&section_ids=23&p[413]=1575`
+let oneBedUrl = `https://www.alo.bg/obiavi/imoti-prodajbi/apartamenti-stai/?region_id=2&location_ids=300&section_ids=23&p[413]=1574`
+let twoBedUrl = `https://www.alo.bg/obiavi/imoti-prodajbi/apartamenti-stai/?region_id=2&location_ids=300&section_ids=23&p[413]=1575`
 let threeBedUrl = `https://www.alo.bg/obiavi/imoti-prodajbi/apartamenti-stai/?region_id=2&location_ids=300&section_ids=23&p[413]=1576`
 
-await getFirstPage(twobedUrl)
+// const oneBedApartments = await getFirstPage(oneBedUrl)
+// const twoBedApartments = await getFirstPage(twoBedUrl)
+const threeBedroomApartments = await getFirstPage(threeBedUrl)
 
-// await getAllProperties()
+// console.log(oneBedApartments)
+// console.log(twoBedApartments)
 
-export default { getOneBedroomApartments, getAllProperties }
+export default { getApartments }
