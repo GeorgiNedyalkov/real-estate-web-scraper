f55cc5181eee7939a3175ecf8cb7731e70b46fc9
GeorgiNedyalkov <gnedyalkov94@gmail.com>
2023-08-04 16:44:05 +0300

    create a get local apartments function

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -3,33 +3,40 @@ import { useState, useEffect } from "react";
 const apiURL = "http://localhost:3001/api/v1/neighborhoods/izgrev";
 
 export const useFetch = (initialURL = apiURL) => {
-  const [apartments, setApartments] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [url, setUrl] = useState(initialURL);
+    const [apartments, setApartments] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [url, setUrl] = useState(initialURL);
 
-  const getApartments = async (url) => {
-    setLoading(true);
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      const apartments = data.neighborhood[0].apartments;
-      setApartments(apartments);
-      setLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    const getApartments = async (url) => {
+        setLoading(true);
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            const apartments = data.neighborhood[0].apartments;
+            setApartments(apartments);
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
 
-  useEffect(() => {
-    // getApartments(url);
-    setLoading(true);
-    fetch("http://127.0.0.1:5174/apartments.json")
-      .then((res) => res.json())
-      .then((result) => {
-        setLoading(false);
-        setApartments(result.neighborhood[0].apartments);
-      });
-  }, [url]);
+        const getLocalApartments = async () => {
+            setLoading(true);
+            try {
+                const res = await fetch(
+                    "http://127.0.0.1:5174/apartments.json"
+                );
+                const result = await res.json();
+                setLoading(false);
+                setApartments(result.neighborhood[0].apartments);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+    };
 
-  return { apartments, loading, setUrl, setApartments };
+    useEffect(() => {
+        getApartments(url);
+    }, [url]);
+
+    return { apartments, loading, setUrl, setApartments };
 };
